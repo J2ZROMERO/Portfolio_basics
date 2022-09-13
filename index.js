6db1e14e93c76addbe2470f0af76fb0b47c6f1e3
amare1990 <amaremek@gmail.com>
2022-09-13 18:22:48 +0300

    Add box shadow and scroll event

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-
+const header = document.querySelector('header');
+ window.addEventListener('scroll', () => {
+   if (window.scrollY > 600) {
+     header.style.boxShadow = '0px 15px 10px -15px gray';
+   } else {
+     header.style.boxShadow = 'none';
+   }
+ });
  
 
  document.querySelector('.menu_hamburguer').addEventListener('click',() =>{
@@ -59,6 +66,47 @@ document.querySelector('.portfolio_go').addEventListener('click',() =>{
         
         
     });
+
+    document.querySelector('.contact_go').addEventListener('click',() =>{
+    
+      const menu = document.querySelector('.quick_acces');
+      const mobile_version = document.querySelector('.mobile_version');
+      const head = document.querySelector('.header_mobile');
+      
+      menu.style.display = 'none';
+      mobile_version.style.visibility = 'visible';
+head.style.visibility = 'visible';
+      window.onscroll = () => { };
+      window.scroll({
+          top:6500,
+          
+          behavior: 'smooth'
+        });
+      
+      
+  });
+
+  document.querySelector('.about_go').addEventListener('click',() =>{
+
+    
+    const menu = document.querySelector('.quick_acces');
+    const mobile_version = document.querySelector('.mobile_version');
+    const head = document.querySelector('.header_mobile');
+    
+    menu.style.display = 'none';
+
+    mobile_version.style.visibility = 'visible';
+    head.style.visibility = 'visible';
+
+
+    window.onscroll = () => { };
+    window.scroll({
+    top: 5700,
+
+    behavior: 'smooth'
+   });
+
+  });
 
 
 
