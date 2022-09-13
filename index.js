899c37b502d9e4b1af903db00a6893a9412ec568
Jhosep <jose-zromero@hotmail.com>
2022-09-13 10:35:42 -0500

    Modify spaces .js file

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const header = document.querySelector('header');
    } else {
      header.style.boxShadow = 'none';
    }
- });
+});
  
 
- document.querySelector('.menu_hamburguer').addEventListener('click',() =>{
+document.querySelector('.menu_hamburguer').addEventListener('click',() =>{
   const menu = document.querySelector('.quick_acces');
   const mobile_version = document.querySelector('.mobile_version');
   const head = document.querySelector('.header_mobile');
   menu.style.display = 'flex';
   menu.style.flexDirection = 'column' ;
-  menu.style.flexWrap = 'wrap' ;
-  
+  menu.style.flexWrap = 'wrap' ;  
   mobile_version.style.visibility = 'hidden';
   head.style.visibility = 'hidden';
     const menu_ul = document.querySelector('.quick_access_ul');
@@ -23,9 +22,7 @@ const header = document.querySelector('header');
     menu_ul.style.background = 'white';
     menu_ul.style.borderRadius= "0 0 10% 10%";
     window.onscroll = () => { window.scroll(0, 0); };
-
-
-   });
+  });
    
 
 
@@ -37,79 +34,48 @@ menu.style.display = 'none';
 mobile_version.style.visibility = 'visible';
 head.style.visibility = 'visible';
 window.onscroll = () => { };
-
-
     });
 
 
 
 document.querySelector('.portfolio_go').addEventListener('click',() =>{
-
-    
         const menu = document.querySelector('.quick_acces');
         const mobile_version = document.querySelector('.mobile_version');
         const head = document.querySelector('.header_mobile');
-        
         menu.style.display = 'none';
         mobile_version.style.visibility = 'visible';
         head.style.visibility = 'visible';
-        
-        
         window.onscroll = () => { };
         window.scroll({
-            
             top: 700,
-            
-            
             behavior: 'smooth'
           });
-        
-        
     });
 
     document.querySelector('.contact_go').addEventListener('click',() =>{
-    
       const menu = document.querySelector('.quick_acces');
       const mobile_version = document.querySelector('.mobile_version');
       const head = document.querySelector('.header_mobile');
-      
       menu.style.display = 'none';
       mobile_version.style.visibility = 'visible';
-head.style.visibility = 'visible';
+      head.style.visibility = 'visible';
       window.onscroll = () => { };
       window.scroll({
-          top:6500,
-          
+          top:6500,    
           behavior: 'smooth'
         });
-      
-      
   });
 
-  document.querySelector('.about_go').addEventListener('click',() =>{
-
-    
+  document.querySelector('.about_go').addEventListener('click',() =>{  
     const menu = document.querySelector('.quick_acces');
     const mobile_version = document.querySelector('.mobile_version');
     const head = document.querySelector('.header_mobile');
-    
     menu.style.display = 'none';
-
     mobile_version.style.visibility = 'visible';
     head.style.visibility = 'visible';
-
-
     window.onscroll = () => { };
     window.scroll({
     top: 5700,
-
     behavior: 'smooth'
    });
-
   });
-
-
-
-
-
-
