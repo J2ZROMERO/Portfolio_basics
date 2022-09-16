8a2012bca50cb0705b931a72b1d0132db95c3945
Jhosep <jose-zromero@hotmail.com>
2022-09-16 15:16:31 -0500

    Modify position alert (not-valid) on form

--- index.js
+++ index.js
@@ -1113,21 +1113,35 @@ const email = document.getElementById('mail');
 const characters = /^[a-z0-9-]+@[a-z0-9]+\.[a-z0-9.]+$/;
 
 document.querySelector('.form_button').addEventListener('click', (event) => {
+
   if (!characters.test(email.value)) {
     const error = document.querySelector('.error');
 
     error.style.display = 'block';
 
-    window.setTimeout(() => {
-      error.style.display = 'none';
-    }, 2000);
+    // window.setTimeout(() => {
+    //   error.style.display = 'none';
+    // }, 2000);
 
     event.preventDefault();
     if (email.value === '') {
       error.style.display = 'none';
     }
+
+  }else{
+    error.style.display = 'none';
   }
 });
+
+
+
+email.addEventListener('input',(event)=>{
+if(characters.test(email.value)){
+  const error = document.querySelector('.error');
+  error.style.display = 'none';
+}
+})
+
 
 const emailD = document.getElementById('mailD');
 const charactersD = /^[a-z0-9-]+@[a-z0-9]+\.[a-z0-9.]+$/;
@@ -1138,9 +1152,9 @@ document.querySelector('.form_buttonD').addEventListener('click', (event) => {
   if (!charactersD.test(emailD.value)) {
     errorD.style.display = 'block';
 
-    window.setTimeout(() => {
-      errorD.style.display = 'none';
-    }, 2000);
+    // window.setTimeout(() => {
+    //   errorD.style.display = 'none';
+    // }, 2000);
 
     event.preventDefault();
     if (emailD.value === '') {
@@ -1148,3 +1162,10 @@ document.querySelector('.form_buttonD').addEventListener('click', (event) => {
     }
   }
 });
+
+emailD.addEventListener('input',(event)=>{
+  if(charactersD.test(emailD.value)){
+    const errorD = document.querySelector('.errorD');
+    errorD.style.display = 'none';
+  }
+  })
